d7987cc141b1a2618e8a374ec5417820cca56858
William Mulenga Chanda <william.chanda@posteo.de>
2023-04-24 12:03:02 +0200

    added a separate styling for 'ul'

--- components/List/StyledListLayout.js
+++ components/List/StyledListLayout.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
-export const StyledList = styled.li`
+export const StyledListItem = styled.li`
   border: 3px solid green;
-  max-width: 768px;
   border-radius: 20px;
   padding: 1rem;
   margin: 0.25rem;
   font-size: 16px;
-  list-style: none;
   font-weight: bold;
+`;
+
+export const StyledList = styled.ul`
+  max-width: 768px;
+  list-style: none;
 `;
--- components/List/index.js
+++ components/List/index.js
@@ -1,18 +1,16 @@
-import { StyledList } from "./StyledList";
+import { StyledListItem, StyledList } from "./StyledListLayout";
 import { events } from "@/resources/events";
 import uuid4 from "uuid4";
 
 export default function List() {
-  console.log("Hello", events);
-
   return (
-    <ul>
+    <StyledList>
       {events.map((event) => (
-        <StyledList key={uuid4()}>
+        <StyledListItem key={uuid4()}>
           {event.name}: {event.event}
           <p> {event.location}</p>
-        </StyledList>
+        </StyledListItem>
       ))}
-    </ul>
+    </StyledList>
   );
 }
